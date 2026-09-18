0125fed4dce4d59ae9eaf87260ae8d06d51a4f7f
Tynchtyk <stu8768@gmail.com>
2026-09-18 22:39:52 +0600

    Delete Start-IslamDini.cmd

--- Start-IslamDini.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set "ISLAMDINI_NODE=node"
-where node >nul 2>nul
-if errorlevel 1 set "ISLAMDINI_NODE=%USERPROFILE%\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe"
-set "ISLAMDINI_PORT=4175"
-start "IslamDini" /min "%ISLAMDINI_NODE%" server.js
-timeout /t 2 /nobreak >nul
-start "" "http://127.0.0.1:4175/"
